c3479f55b54b926292c8e4dc4c689dcdcc113864
micho <gatin13@yahoo.com>
2015-01-07 19:23:19 +0900

    Set landing page layout. Added Spanish i18n support. Created basic menu

--- app/src/main/java/jp/co/nekosoft/underboss/MainActivity.java
+++ app/src/main/java/jp/co/nekosoft/underboss/MainActivity.java
@@ -1,17 +1,33 @@
 package jp.co.nekosoft.underboss;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    private Button createArmyListButton;
+    private Button collectionButton;
+    private Button referenceButton;
+    private Button statisticsButton;
+    private Button battleDiaryButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createArmyListButton = (Button) findViewById(R.id.create_army_list_button);
+        collectionButton = (Button) findViewById(R.id.collection_button);
+        referenceButton = (Button) findViewById(R.id.reference_button);
+        statisticsButton = (Button) findViewById(R.id.statistics_button);
+        battleDiaryButton = (Button) findViewById(R.id.battle_diary_button);
     }
 
 
@@ -36,4 +52,30 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void onCreateArmyListButtonClick(View view) {
+//        TODO: Implement the button onClick logic
+        Toast.makeText(this, R.string.create_army_list_button, Toast.LENGTH_SHORT).show();
+    }
+    
+    public void onCollectionButtonClick(View view) {
+//        TODO: Implement the button onClick logic
+        Toast.makeText(this, R.string.collection_button, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onReferenceButtonClick(View view) {
+//        TODO: Implement the button onClick logic
+        Toast.makeText(this, R.string.reference_button, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onStatisticsButtonClick(View view) {
+//        TODO: Implement the button onClick logic
+        Toast.makeText(this, R.string.statistics_button, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onBattleDiaryButtonClick(View view) {
+//        TODO: Implement the button onClick logic
+        Toast.makeText(this, R.string.battle_diary_button, Toast.LENGTH_SHORT).show();
+    }
+
 }
