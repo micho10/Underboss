33247dc52b95b839b8e57bd2ea1637deddf4422e
micho <gatin13@yahoo.com>
2015-01-08 23:29:01 +0900

    Battle report screen wip

--- app/src/main/java/jp/co/nekosoft/underboss/MainActivity.java
+++ app/src/main/java/jp/co/nekosoft/underboss/MainActivity.java
@@ -10,6 +10,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import jp.co.nekosoft.underboss.collection.CollectionActivity;
+
 
 public class MainActivity extends Activity {
 
